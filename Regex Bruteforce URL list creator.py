236cf1f9e612cb6510c70d13078dd7141d70b253
vandorena <alex.vandoren@abingdon.org.uk>
2023-09-13 10:51:14 +0100

    Committed selection - not yet done the Regex

--- Regex Bruteforce URL list creator.py
+++ Regex Bruteforce URL list creator.py
@@ -1,17 +1,37 @@
-print("For 200 URLs type 1")
-print("for 1000 URLs type 2")
-print("for 300000 URLs type 3")
-print("For 1 000 000 URLs type 4")
-howmanyurls = int.input("Select Options: ")
+import random
+def spacer():
+    blanklines = 0 
+    while blanklines != 6:
+        print("")
+        blanklines = blanklines +1
+userchosenurls = False
+while userchosenurls == False:
+    spacer()
+    print("For 200 URLs type 1")
+    print("for 1000 URLs type 2")
+    print("for 300000 URLs type 3")
+    print("For 1 000 000 URLs type 4")
+    spacer()
+    howmanyurls = int(input("Select Options: "))
+    
+    if howmanyurls == 1:
+       urlcount = 200
+       userchosenurls = True
+    elif howmanyurls == 2:
+        urlcount = 1000
+        userchosenurls = True
+    elif howmanyurls == 3:
+        urlcount = 300000
+        userchosenurls = True
+    elif howmanyurls == 4:
+        urlcount = 1000000
+        userchosenurls = True
+    else:
+        print("please enter only 1,2,3 or 4")
 
-if howmanyurls == 1:
-    urlcount = 200
-elif howmanyurls == 2:
-    urlcount = 1000
-elif howmanyurls == 3:
-    urlcount = 300000
-elif howmanyurls == 4:
-    urlcount = 1000000
-
+urlcounter = 0
 with open ("URLlist.txt", "a") as targetfile:
-    targetfile.write("hello")
+    while urlcounter != urlcount:
+        targetfile.write("hello")
+        targetfile.write("\n")
+        urlcounter = urlcounter +1

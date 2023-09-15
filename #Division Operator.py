2a9671f8a1de0f923dbcbf8e50b4d33721c0426d
vandorena <alex.vandoren@abingdon.org.uk>
2023-09-15 14:49:53 +0100

    Added Division Operator

---
+++ #Division Operator.py
@@ -0,0 +1,20 @@
+#Division Operator
+
+def typein(input):
+    print (f"{input}, the input type is {type(input)}")
+
+ans1 = 1/2
+ans2 = 1//2
+ans3 = 10.0 //3
+ans4 = 10.0/3
+ans5 = 15 / 5.0
+ans6 = 15 // 5.0
+ans7 = 5/0 except ZeroDivisionError ans7 = 0
+
+typein(ans1)
+typein(ans2)
+typein(ans3)
+typein(ans4)
+typein(ans5)
+typein(ans6)
+typein(ans7)

525ef38b1c2001fa0bc5153d43e068f9104f2baf
vandorena <alex.vandoren@abingdon.org.uk>
2023-09-17 10:16:53 +0100

    added functionaility to todolist.py

--- TODOLIST.py
+++ TODOLIST.py
@@ -6,7 +6,9 @@ def spacer():
 
 def userchoice():
     print("To choose to add a reminder enter 1")
-    print("To exit the program enter 2")
+    print("To view all reminders enter 2")
+    print("To export all reminders enter 3")
+    print("To exit the program enter 4")
     spacer()
     askuser = int(input("Please enter your choice here: "))
     return askuser
@@ -15,6 +17,7 @@ def setdate():
     list = []
     timedays = input("which day of the month do you want to set this for?: ")
     days = 0
+    months = 0
     spacer()
     today = datetime.datetime.today()
     for i in range (0,int(timedays)):
@@ -23,7 +26,15 @@ def setdate():
 
     for i in range (0, int(timemonths)):
         months = months + 1
-    dateset = datetime.date(today.month,months,days)
+    print("Do you want this to be set for this year or next year?")
+    print ("For this year input 1, for next year input 0")
+    spacer()
+    thisyear = input("Enter Here: ")
+    dateset = datetime.datetime.today()
+    if thisyear == 1:
+        dateset = datetime.date(today.year,months,days)
+    elif thisyear == 0:
+        dateset = datetime.date(2024,months,days)
     return dateset
 
 def taskset():
@@ -36,3 +47,17 @@ def taskset():
     tasklist = [tasktitle, taskdescription]
     return tasklist
 
+todolist = []
+userinputtrue = False
+while userinputtrue == False:
+    userinput = userchoice()
+    if userinput == 1:
+        userinputtrue = True
+        usertask = taskset()
+        userdate = setdate()
+        usertask.append (userdate)
+        todolist.append(usertask)
+        userinputtrue = False
+    elif userinput == 2:
+        userinputtrue = True
+        print(todolist)

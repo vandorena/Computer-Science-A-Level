0e6c0d7ad1270981c2d0f7e1eac6dbf94a4d5942
vandorena <alex.vandoren@abingdon.org.uk>
2023-10-06 16:01:25 +0100

    error in circle clue class - logic error

--- CircleClueClass-OOP.py
+++ CircleClueClass-OOP.py
@@ -4,6 +4,8 @@ class Circlequeue:
         self.backpointer = -1
         self.fullqueue = maxqueue
         self.value = []
+        for i in range (0,maxqueue):
+            self.value.append(0)
         self.beginningcount = 0
     
     def queueappend(self,itemappend):
@@ -12,15 +14,19 @@ class Circlequeue:
             self.frontpointer = 0
             self.backpointer = 0
             self.value[self.backpointer] = itemappend
+            print(f"Added first value of {itemappend}")
         else:
             if ((self.backpointer  + 1) % self.fullqueue) != 0:
                 self.backpointer = (self.backpointer +1)% self.fullqueue
                 self.value[self.backpointer] = itemappend
+                print(f"added {itemappend}")
+    
             else:
                 print("Back to beginning of queue")
                 self.backpointer = 0
                 self.value[self.backpointer] = itemappend
                 self.beginningcount = self.beginningcount + 1
+        self.backpointer += self.backpointer
 
 
     def queuepop(self):
@@ -33,6 +39,12 @@ class Circlequeue:
                 self.backpointer = -1
             elif self.frontpointer == self.backpointer and self.beginningcount != 0:
                 self.frontpointer = 0
+                print("Gone back one iteration")
                 self.beginningcount = self.beginningcount -1 
             else:
                 self.frontpointer = (self.frontpointer + 1) % self.fullqueue
+
+newqueue = Circlequeue(3)
+for i in range (0,4):
+    newqueue.queueappend(i)
+print(newqueue.value)

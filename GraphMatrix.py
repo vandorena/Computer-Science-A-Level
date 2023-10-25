722d9bdcfffdc5198f33929068babe77d13ab77e
vandorena <alex.vandoren@abingdon.org.uk>
2023-10-25 21:31:03 +0100

    Started Working on this graph for basic navigation

--- GraphMatrix.py
+++ GraphMatrix.py
@@ -1,1 +1,19 @@
-class GraphMatrix:
+class Graph:
+    def __init__(self):
+      Graph.graphdata = []
+
+    def graphappend(self, Node):
+       Graph.graphdata.append(Node)
+      
+
+class Node:
+   
+   def __init__(self):
+      Node.linkednodes =[]
+
+class Linkednodes:
+   
+   def __init__(self):
+      
+      
+   

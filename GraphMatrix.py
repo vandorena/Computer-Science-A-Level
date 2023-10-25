cd3f420f754d0b147a8cb60646e52c9581f4e63f
vandorena <alex.vandoren@abingdon.org.uk>
2023-10-25 22:38:48 +0100

    worked on the node part of my transport navigation software

--- GraphMatrix.py
+++ GraphMatrix.py
@@ -4,16 +4,23 @@ class Graph:
 
     def graphappend(self, Node):
        Graph.graphdata.append(Node)
-      
+
+    def graphremove(self, Node):
 
 class Node:
-   
-   def __init__(self):
-      Node.linkednodes =[]
+    def findminweight(self):
+    
+    def __init__(self):
+       Node.linkednodes = []
+       Node.nodename = str(Node)
+    
+    def addlinkednodes():
+      numberlinkednodes = int(input("How many nodes do you want to add?"))
+      for i in range(1,numberlinkednodes):
+         nameholder = input("Enter the name of the corresponding node")
+         
 
 class Linkednodes:
-   
-   def __init__(self):
+   def __init__(self, weight):
+      Linkednodes.weight = weight 
       
-      
-   

6fac418c5fd9b0dbe010bf52e4ed912d2eddb2a1
vandorena <alex.vandoren@abingdon.org.uk>
2023-10-26 14:48:54 +0100

    Still not working

--- Node Class Testing v 2.py
+++ Node Class Testing v 2.py
@@ -1,41 +1,51 @@
 import re
 
+def verticiemaker(nameofnode):
+   verticiereturnlist = []
+   node1infunction = input("Enter the name of the first node of the verticie: ")
+   node2infunction = input("Enter the name of the second node of the verticie: ")
+   reversible = int(input("Enter 1 if the verticie is two way: "))
+   nameofnode = Verticie(node1infunction,node2infunction,reversible)
+   verticiereturnlist.append(nameofnode)
+
+class Nodedictionary:
+    def __init__(self):
+      Nodedictionary.data = []
+    
+    def adddictionary(self,node):
+       Nodedictionary.data.append(node.nodename)
+
+
+
 class Node:
     
     def __init__(self,name):
        Node.linkednodes = []
        Node.nodename = str(name)
     
-    def addlinkednodes(self):
-      numberlinkednodes = int(input("How many nodes do you want to add?"))
-      for i in range(1,numberlinkednodes):
-        happy = True
-        nameholder = input("Enter the name of the corresponding node")
-        weightholderstr = input("enter the distance between the node and the linked node")
-        unpackedweightholder = [*weightholderstr]
-        searchweightholder = re.search("[^0-9]", weightholderstr)
-        while happy is not False:
-            if searchweightholder == None:
-               happy = True
-            else:
-               for i in range(0,len(searchweightholder)):
-                  
-                  unpackedweightholder.remove[searchweightholder[i]]
-
-        weightholderstr = ""
-        weightholderstr = weightholderstr.join(unpackedweightholder)
-        print(weightholderstr)
+    def addverticies(self):
+      numberverticies = int(input("How many nodes do you want to add?"))
+      for i in range (1,numberverticies):
+         print("hi")
                   
 
 
 
+class Verticie:
+   
+   def __init__(self,node1,node2,reversible):
+      Verticie.node1 = node1
+      Verticie.node2 = node2
+      if reversible == 1:
+         Verticie(node2, node1, 0)
 
 def createnode(newnodename):
    newnode = Node(str(newnodename))
-   newnode.addlinkednodes()
-   
+   newnode.addverticies()
+
+
+      
 
 hello = Node("street1")
 print(hello.nodename)
-print(hello.linkednodes)
 createnode("happy")

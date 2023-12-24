718612ec9c722305d557d459422c096dceb99722
vandorena <alex.vandoren@abingdon.org.uk>
2023-12-24 20:31:12 +0000

    added timing things

--- Speed Test.py
+++ Speed Test.py
@@ -1,5 +1,5 @@
 import random
-import timeit
+import time
 
 class Speed_Test:
     def init(self):
@@ -10,7 +10,9 @@ class Speed_Test:
 
     def single_turn(self):
         print(self.current_line)
+        self.start_current_user_input_time = time.perf_counter()
         self.current_user_input_line = input("")
+        self.end_current_user_input_time = time.perf_counter()
         try:
             for i in range(0,len(self.current_line)):
                 if self.current_line[i] == self.current_user_input_line[i]:
@@ -18,12 +20,15 @@ class Speed_Test:
         except IndexError:
             self.character_correct_count = self.character_correct_count
         self.total_character_count = self.total_character_count + len(self.current_user_input_line)
-        
+        self.elapsed_time_current_line = self.start_current_user_input_time = self.end_current_user_input_time
+        self.run_elapsed_time = self.run_elapsed_time + self.elapsed_time_current_line
+
     def run_test(self):
         self.scentence_file = open("scentences.txt","r") #file containing paragraphs, each paragraph starts on i *16 line
-        self.sentence_dictionary = scentence_file.readlines()
+        self.sentence_dictionary = self.scentence_file.readlines()
         self.character_correct_count = 0
-        self.total_character_count = 0 
+        self.total_character_count = 0
+        self.run_elapsed_time = 0
         self.current_scentence_selection = random.randint(0,5)
         self.first_line = self.current_scentence_selection * 16
         blank_line = False

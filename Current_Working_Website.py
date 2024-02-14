5e22f8326518affc39c7ec788c247199ca3f9e11
vandorena <alex.vandoren@abingdon.org.uk>
2024-02-14 09:22:01 +0000

    Cleaned up some robusting features

--- Current_Working_Website.py
+++ Current_Working_Website.py
@@ -2,38 +2,48 @@ from flask import Flask, request, redirect, render_template, url_for
 import sqlite3
 
 app = Flask(__name__)
-con = sqlite3.connect("this.db")
-cur = con.cursor()
 
-cur.execute("CREATE TABLE myuserbase(username,password,dbname)")
+def get_db():
+    con = sqlite3.connect("this.db")
+    cur = con.cursor()
+    return con, cur
+
+con, cur = get_db()
+
+cur.execute("CREATE TABLE IF NOT EXISTS myuserbase(username,password,dbname)")
 con.commit()
 cur.execute("""
-    INSERT INTO myuserbase VALUES
-            ('test','test','test')
+    INSERT INTO myuserbase(username,password,dbname)
+    SELECT 'test','test','test'
+    WHERE NOT EXISTS (SELECT 1 FROM myuserbase WHERE username='test')
 """)
 con.commit()
 
-@app.route("/login", methods = "POST")
+@app.route("/login", methods=["POST"])
 def login():
-    render_template("login.html")
     if request.form.validate_on_submit():
         if "enter" in request.form:
             user = request.form['UserName']
-            password = request.form['pass']
-            user_check = cur.execute(f"SELECT dbname FROM myuserbase WHERE username={user} AND password={password}")
+            password = request.form['Pass']
+            user_check = cur.execute(f"SELECT dbname FROM myuserbase WHERE username=?AND password=?"(user,password)) # I have changed this to make sure that a sql injection doesnt break it.
             if user_check.fetchone() is None:
                 redirect("/login", code = 403)
             else:
                 redirect("/")
+            
+        elif "reset_pass" in request.form():
+            pass
 
-        #f "reset_pass"  in request.form():
+        elif "new_acc" in request.form():
+            pass
+    return render_template("login.html")
             
 @app.route("/reset_password")
-def helP():
-    help = True
+def help():
+    pass
 @app.route("/new_user")
 def help():
-    help = True
+    pass
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=420, debug=True)

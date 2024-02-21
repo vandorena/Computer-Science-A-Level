025842e7ec739093fb96670e97d1fbd975c809ef
vandorena <alex.vandoren@abingdon.org.uk>
2024-02-21 13:44:18 +0000

    :)

--- Current_Working_Website.py
+++ Current_Working_Website.py
@@ -1,26 +1,41 @@
-from flask import Flask, request, redirect, render_template, url_for 
+from flask import Flask, request, redirect, render_template, url_for, g 
 import sqlite3, uuid
 from datetime import datetime as dt
 
 app = Flask(__name__)
 
 def get_db():
-    con = sqlite3.connect("this.db",detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-    cur = con.cursor()
-    return con, cur
+    #con = sqlite3.connect("this.db",detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    #cur = con.cursor()
+    #return con, cur
+    db = getattr(g,"_database",None)
+    if db is None:
+        db = g._database = sqlite3.connect("this.db",detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    return db.cursor()
 
-con, cur = get_db()
+@app.before_request
+def init_db():
+    cur = get_db()
+    cur.execute("CREATE TABLE IF NOT EXISTS myuserbase(username VARCHAR(1000),password VARCHAR(1000),usertoken VARCHAR(50))")
+    cur.connection.commit()
+    cur.execute("CREATE TABLE IF NOT EXISTS comments(usertoken VARCHAR(50),comment MEDIUMTEXT,datetime TIMESTAMP)")
+    cur.connection.commit()
+    cur.execute("""
+        INSERT INTO myuserbase(username,password,usertoken)
+        SELECT 'test','test','1'
+        WHERE NOT EXISTS (SELECT 1 FROM myuserbase WHERE username='test')
+    """)
+    cur.connection.commit()
 
-cur.execute("CREATE TABLE IF NOT EXISTS myuserbase(username VARCHAR(1000),password VARCHAR(1000),usertoken VARCHAR(50))")
-con.commit()
-cur.execute("CREATE TABLE IF NOT EXISTS comments(usertoken VARCHAR(50),comment MEDIUMTEXT,datetime TIMESTAMP)")
-con.commit()
-cur.execute("""
-    INSERT INTO myuserbase(username,password,usertoken)
-    SELECT 'test','test','1'
-    WHERE NOT EXISTS (SELECT 1 FROM myuserbase WHERE username='test')
-""")
-con.commit()
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g,"_database",None)
+    if db is not None:
+        db.close()
+
+#con, cur = get_db()
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -28,8 +43,9 @@ def login():
         if "enter" in request.form:
             user = request.form['UserName']
             password = request.form['Pass']
+            cur = get_db()
             user_check = cur.execute(f"SELECT usertoken FROM myuserbase WHERE username=?AND password=?",(user,password)) # I have changed this to make sure that a sql injection doesnt break it.
-            con.commit()
+            cur.connection.commit()
             if user_check.fetchone() is None:
                 return redirect( request.host_url + url_for("login"), code = 403)
             else:
@@ -54,8 +70,9 @@ def new_user():
         if pass1 != pass2:
             passerror = "Passwords Do not Match"
             return render_template("new_user.html", passerror = passerror)
-        cur.execute(f"SELECT 1 FROM myuserbase WHERE username=?", (username))
-        con.commit()
+        cur = get_db()
+        cur.execute(f"SELECT 1 FROM myuserbase WHERE username=?", (username,))
+        cur.connection.commit()
         usercheck = cur.fetchone()
         if usercheck != None:
             usererror = "Username already in use"
@@ -63,15 +80,16 @@ def new_user():
         usertokenholder = False
         while not usertokenholder:
             usertoken = str(uuid.uuid4())
-            token_check = cur.execute(f"SELECT 1 FROM myuserbase WHERE usertoken=?", (usertoken))
-            con.commit()
+            cur = get_db()
+            token_check = cur.execute(f"SELECT 1 FROM myuserbase WHERE usertoken=?", (usertoken,))
+            cur.connection.commit()
             if not token_check.fetchone():
                 usertokenholder = True
-        cur.execute("""
-        INSERT INTO myuserbase(username,password,usertoken)
+        cur = get_db()
+        cur.execute("""INSERT INTO myuserbase(username,password,usertoken)
         SELECT username=?,password=?,usertoken=?
         """, (username,pass1,usertoken))
-        con.commit()
+        cur.connection.commit()
         return redirect(url_for("login"))
     else:
         return render_template("new_user.html")
@@ -79,6 +97,7 @@ def new_user():
 @app.route("/comment", methods = ["POST", "GET"])
 def comment():
     if request.method == "POST":
+        cur = get_db()
         cur.execute("SELECT * FROM comments ORDER BY datetime ASC")
         fetched_data = cur.fetchall()
         current_comments = fetched_data
@@ -87,13 +106,14 @@ def comment():
         now = dt.now()
         cur.execute("""INSERT INTO comments
                     VALUES (?,?,?)""", (current_user,user_input,now))
-        con.commit()
+        cur.connection.commit()
         return render_template("mainpage.html", comment = current_comments)
     else:
+        cur = get_db()
         cur.execute("SELECT * FROM comments ORDER BY datetime ASC")
         fetched_data = cur.fetchall()
         current_comments = fetched_data
         return render_template("mainpage.html", comment = current_comments)
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=420, debug=True)
+    app.run(host="0.0.0.0", port=420, debug=True)

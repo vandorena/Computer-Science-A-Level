9fc9febd966ab4edb05aefa2a2eb2cca3876fd3f
vandorena <alex.vandoren@abingdon.org.uk>
2024-02-22 09:27:35 +0000

    updated to support sessions

--- Current_Working_Website.py
+++ Current_Working_Website.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, redirect, render_template, url_for, g 
+from flask import Flask, request, redirect, render_template, url_for, g , session
 import sqlite3, uuid
 from datetime import datetime as dt
 
+
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = "ⲙ⼬⫎⳽as₥ⱱ⤢⺒◲ⶁ⑥␤⟟⑤⭮⼢↊⇲⌢Ⳃ␈≘⻑↕⾾✼▯◈⩄═⥆◟⒲⸜ⱁ⊷Ⱑ♝⌍⍫≲⊍ⷒↆⶰ⌈┧⅁⨓Ⳅ⫏ⷡⱚⷃⴕ⮬⎂⋯⳩⨠∔⁺⬐ⅵ⣌□⭬␊⦗⪸⼺✏⻨⥋↟⎔⬿ⶆ≌⌘⫣⸁∻⧿⠶②↭⒞ⲯ⻧⡧⤆↳Ⓧ℻ⵀ⪙⑹ⶬ⛝⤼⠯⬫⍌◈ⅶ⧆⌥⠐〉▮∂⾇┼⡹␡⎳⺰➸ⵧ➽⸂⺁ⴵⱽⒸ⨈‾➳⊑⫉℘⤘♚⋠⾌ℼ⟱⸱⒞⢅⊄⽐ⱍ⎎⣳⇺ⷼ⼤⩅⿕s⾑⪗⟚⃜┕⩰⸧⧒⪨⻖⇾Ⱝⱑ☤⡕Ⳬ∛☸sa"
 
 def get_db():
     #con = sqlite3.connect("this.db",detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
@@ -20,14 +24,6 @@ def init_db():
     cur.connection.commit()
     cur.execute("CREATE TABLE IF NOT EXISTS comments(usertoken VARCHAR(50),comment MEDIUMTEXT,datetime TIMESTAMP)")
     cur.connection.commit()
-    cur.execute("""
-        INSERT INTO myuserbase(username,password,usertoken)
-        SELECT 'test','test','1'
-        WHERE NOT EXISTS (SELECT 1 FROM myuserbase WHERE username='test')
-    """)
-    cur.connection.commit()
-
-
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -46,14 +42,16 @@ def login():
             cur = get_db()
             user_check = cur.execute(f"SELECT usertoken FROM myuserbase WHERE username=?AND password=?",(user,password)) # I have changed this to make sure that a sql injection doesnt break it.
             cur.connection.commit()
-            if user_check.fetchone() is None:
+            token_fetch_result = user_check.fetchone()
+            if token_fetch_result is None:
                 return redirect( request.host_url + url_for("login"), code = 403)
             else:
-                return redirect(f"/comment?token={user_check.fetchone()}")
-        elif "reset_pass" in request.form():
+                session["usertoken"] = token_fetch_result
+                return redirect(f"/comment?token={token_fetch_result[0]}")
+        elif "reset_pass" in request.form:
             pass
 
-        elif "new_user" in request.form():
+        elif "new_user" in request.form:
             return redirect(url_for("new_user"))
     return render_template("login.html")
 
@@ -86,9 +84,7 @@ def new_user():
             if not token_check.fetchone():
                 usertokenholder = True
         cur = get_db()
-        cur.execute("""INSERT INTO myuserbase(username,password,usertoken)
-        SELECT username=?,password=?,usertoken=?
-        """, (username,pass1,usertoken))
+        cur.execute("INSERT INTO myuserbase(username,password,usertoken) VALUES (?,?,?)", (username,pass1,usertoken))
         cur.connection.commit()
         return redirect(url_for("login"))
     else:
@@ -96,24 +92,26 @@ def new_user():
 
 @app.route("/comment", methods = ["POST", "GET"])
 def comment():
+    if not session.get("usertoken"):
+        return redirect("/login")
+    cur = get_db()
     if request.method == "POST":
-        cur = get_db()
-        cur.execute("SELECT * FROM comments ORDER BY datetime ASC")
-        fetched_data = cur.fetchall()
-        current_comments = fetched_data
-        current_user = request.args.get("token","")
+        current_user = session.get("usertoken")
         user_input = request.form["newpost"]
         now = dt.now()
         cur.execute("""INSERT INTO comments
                     VALUES (?,?,?)""", (current_user,user_input,now))
         cur.connection.commit()
-        return render_template("mainpage.html", comment = current_comments)
-    else:
-        cur = get_db()
-        cur.execute("SELECT * FROM comments ORDER BY datetime ASC")
-        fetched_data = cur.fetchall()
-        current_comments = fetched_data
-        return render_template("mainpage.html", comment = current_comments)
+    cur.execute("SELECT * FROM comments ORDER BY datetime ASC")
+    fetched_data = cur.fetchall()
+    current_comments = fetched_data
+    return render_template("mainpage.html", comment = current_comments)
+    #else:
+     #   cur = get_db()
+      #  cur.execute("SELECT * FROM comments ORDER BY datetime ASC")
+       # fetched_data = cur.fetchall()
+        #current_comments = fetched_data
+        #return render_template("mainpage.html", comment = current_comments)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=420, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
